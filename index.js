53366c91bbb13eeae6120e98ae60284718dbf023
uilse-lang <khorolgarav.b0528@gmail.com>
2024-11-16 21:29:31 -0800

    hhe

---
+++ index.js
@@ -0,0 +1,11 @@
+for(var i=1; i<4; i++){
+    console.log(i)
+}
+console.log("*")
+for(var i=i+1; i<7; i++){
+    console.log(i)   
+}
+console.log("!")
+for(var i=i+1; i<10; i++){
+    console.log(i)
+}console.log("&")
